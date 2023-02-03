8d326dbd4e36168311938a3ea30be7cd1d1b640a
Josh Musto <musto.josh@gmail.com>
2023-02-02 22:05:04 -0500

    pass04

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const path = require('path');
 const notes = require("./routes/notes.js");
-
 const PORT = process.env.PORT || 3001;
 
+//set a variable for express
 const app = express();
 
 app.use(express.static('public'));
 
+//whenever '/api' is used in a URL, reference the file set to [notes], in this case "./routes/notes.js"
 app.use("/api", notes);
 
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-//GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
